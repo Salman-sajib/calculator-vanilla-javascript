e40180bca35fe3cce2c9dda1e0a95289f45d1f22
Salman-sajib <sfsajib@gmail.com>
2023-11-25 12:32:36 +0600

    Enhanced code organization, added useful comments, and improved clarity for better readability. Additionally, refactored the mode toggle function for simplicity.

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
+// Initialize mode toggle functionality
 import { initModeToggle } from "./modeToggle";
-
 const body = document.querySelector("body");
 const toggleButton = document.querySelector(".btn-toggle");
+initModeToggle(body, toggleButton);
 
+// Calculator elements
 const calculator = document.querySelector(".calculator");
 const keys = calculator.querySelector(".calculator__keys");
 const display = calculator.querySelector(".calculator__display");
 
-initModeToggle(body, toggleButton);
-
+// Set initial display value
 updateDisplay("0");
 
+// Event listener for key clicks
 keys.addEventListener("click", handleKeyClick);
 
 function handleKeyClick(event) {
@@ -22,31 +24,38 @@ function handleKeyClick(event) {
   const { type } = key.dataset;
   const { previousKeyType } = calculator.dataset;
 
+  // Handle number and decimal key clicks
   if (type === "number" || type === "decimal") {
     handleNumberClick(keyValue, displayValue, previousKeyType);
   }
 
+  // Handle operator key click
   if (type === "operator") {
     handleOperatorClick(key, displayValue);
   }
 
+  // Handle equal key click
   if (type === "equal") {
     handleEqualClick(displayValue);
   }
 
+  // Handle clear key click
   if (type === "clear") {
     handleClearClick();
   }
 
+  // Update the previous key type
   calculator.dataset.previousKeyType = type;
 }
 
+// Update display with a given value
 function updateDisplay(value) {
-  // Limit the display to 10 digits.
+  // Limit the display to 10 digits
   value = value.slice(0, 10);
   display.textContent = value;
 }
 
+// Handle number key clicks
 function handleNumberClick(keyValue, displayValue, previousKeyType) {
   if (
     displayValue === "0" ||
@@ -55,6 +64,7 @@ function handleNumberClick(keyValue, displayValue, previousKeyType) {
   ) {
     updateDisplay(keyValue);
   } else {
+    // Avoid appending a decimal point if it already exists
     if (keyValue === "." && displayValue.includes(".")) {
       return;
     }
@@ -62,20 +72,24 @@ function handleNumberClick(keyValue, displayValue, previousKeyType) {
   }
 }
 
+// Handle operator key click
 function handleOperatorClick(key, displayValue) {
   clearStateFromOperator();
   key.dataset.state = "selected";
 
+  // Store the first number and operator in dataset attributes
   calculator.dataset.firstNumber = displayValue;
   calculator.dataset.operator = key.dataset.key;
 }
 
+// Handle equal key click
 function handleEqualClick(displayValue) {
   const firstNumber = calculator.dataset.firstNumber;
   const operator = calculator.dataset.operator;
   const secondNumber = displayValue;
 
   if (firstNumber && operator) {
+    // Perform the calculation, update display, and clear operator state
     const result = calculate(firstNumber, operator, secondNumber);
     updateDisplay(result);
     calculator.dataset.firstNumber = result;
@@ -84,13 +98,16 @@ function handleEqualClick(displayValue) {
   clearStateFromOperator();
 }
 
+// Handle clear key click
 function handleClearClick() {
+  // Reset display and clear dataset attributes and operator state
   updateDisplay("0");
   calculator.dataset.firstNumber = "";
   calculator.dataset.operator = "";
   clearStateFromOperator();
 }
 
+// Clear the selected state from operator keys
 function clearStateFromOperator() {
   const operatorKeys = keys.querySelectorAll(`[data-type="operator"]`);
   operatorKeys.forEach((el) => {
@@ -98,17 +115,19 @@ function clearStateFromOperator() {
   });
 }
 
+// Perform the calculation based on the operator
 function calculate(firstNumber, operator, secondNumber) {
   firstNumber = parseFloat(firstNumber);
   secondNumber = parseFloat(secondNumber);
 
   let result = 0;
+  // Perform the calculation based on the operator type
   if (operator === "plus") result = firstNumber + secondNumber;
   if (operator === "minus") result = firstNumber - secondNumber;
   if (operator === "times") result = firstNumber * secondNumber;
   if (operator === "divide") result = firstNumber / secondNumber;
 
-  // Limit the result to 10 digits and remove trailing zeros.
+  // Limit the result to 10 digits and remove trailing zeros
   result = result.toFixed(10).replace(/\.?0+$/, "");
 
   return result;
--- modeToggle.js
+++ modeToggle.js
@@ -1,16 +1,14 @@
 export function initModeToggle(element, toggleButton) {
+  // Retrieve dark mode preference from local storage
   let isDarkMode = localStorage.getItem("dark") === "true";
 
   function setMode(isDark) {
-    if (isDark) {
-      element.dataset.colorMode = "dark";
-      toggleButton.textContent = "Light";
-    } else {
-      element.dataset.colorMode = "light";
-      toggleButton.textContent = "Dark";
-    }
+    // Set dataset attribute and button text based on color mode
+    element.dataset.colorMode = isDark ? "dark" : "light";
+    toggleButton.textContent = isDark ? "Light" : "Dark";
 
     isDarkMode = isDark;
+
     localStorage.setItem("dark", isDark);
   }
 
